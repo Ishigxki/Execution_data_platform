e0aefa8c8a9e90b809f53c6f04e5605a03b813eb
Bonga Maseko <102910937+Ishigxki@users.noreply.github.com>
2026-05-05 12:46:57 +0200

    Update database connection to use DATABASE_URL

--- database.py
+++ database.py
@@ -2,5 +2,4 @@ import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(os.environ["postgresql://postgres:HmABJHJaHJLadzMGIyvIZhtazTEpZpVq@postgres.railway.internal:5432/railway"])
-
+    return psycopg2.connect(os.environ["DATABASE_URL"])
