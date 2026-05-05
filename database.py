66e52c6e8a045db7337a18d1790e07ef3525e873
Bonga Maseko <102910937+Ishigxki@users.noreply.github.com>
2026-05-05 12:46:13 +0200

    Update database connection to use environment variable

--- database.py
+++ database.py
@@ -1,11 +1,6 @@
+import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(
-    host = "localhost",
-    database = "postgres",
-    user = "postgres",
-    password = "buru@god"
-)
-
+    return psycopg2.connect(os.environ["postgresql://postgres:HmABJHJaHJLadzMGIyvIZhtazTEpZpVq@postgres.railway.internal:5432/railway"])
 
